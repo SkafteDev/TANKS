2ac37d5276ba76f998824ea22c01fa4e9a7925d1
Unknown <victor.s@live.dk>
2018-03-12 12:01:29 +0100

    JavaDocs

--- dk.grp1.tanks.common/src/main/java/dk/grp1/tanks/common/data/parts/MovementPart.java
+++ dk.grp1.tanks.common/src/main/java/dk/grp1/tanks/common/data/parts/MovementPart.java
@@ -17,37 +17,83 @@ public class MovementPart implements IEntityPart {
 
     }
 
+    /**
+     * Returns the current speed in m/s
+     * @return
+     */
     public float getCurrentSpeed(){
         return velocity.getLength();
     }
 
+    /**
+     * Returns the velocity (directional speed) as a Vector
+     * @return
+     */
     public Vector2D getVelocity() {
         return velocity;
     }
 
+    /**
+     * Sets the velocity
+     * @param x change in horizontal position in m/s
+     * @param y change in vertical position in m/s
+     */
     public void setVelocity(float x, float y){
-        this.velocity = new Vector2D(x,y);
+        setVelocity( Vector2D(x,y));
     }
 
+    /**
+     * Sets the velocity
+     * @param velocity vector containing the change in horizontal and vertical position in m/s
+     */
     public void setVelocity(Vector2D velocity) {
+        if(velocity.getLength() > getMaxSpeed()){
+            double ratio = velocity.getLength() / getMaxSpeed();
+            velocity.setX(velocity.getX()/ratio);
+            velocity.setY(velocity.getY()/ratio);
+        }
         this.velocity = velocity;
+
+
     }
 
+    /**
+     * Returns the acceleration
+     * @return vector containing the change in horizontal and vertical velocity in (m/s)/s
+     */
     public Vector2D getAcceleration() {
         return acceleration;
     }
 
+    /**
+     * Sets the acceleration
+     * @param x change in horizontal velocity in (m/s)/s
+     * @param y change in vertical velocity in (m/s)/s
+     */
     public void setAcceleration(float x, float y){
         this.acceleration = new Vector2D(x,y);
     }
+
+    /**
+     * Sets the acceleration
+     * @param acceleration vector containing the change in horizontal and vertical velocity in (m/s)/s
+     */
     public void setAcceleration(Vector2D acceleration) {
         this.acceleration = acceleration;
     }
 
+    /**
+     * Get the maximum allowed speed
+     * @return
+     */
     public float getMaxSpeed() {
         return maxSpeed;
     }
 
+    /**
+     * Sets the maximum allowed speed
+     * @param maxSpeed
+     */
     public void setMaxSpeed(float maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
