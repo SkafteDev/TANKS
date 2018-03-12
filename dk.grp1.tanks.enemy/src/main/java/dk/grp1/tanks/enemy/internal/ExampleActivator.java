d9ae792b86c6ab926f7c88c3a13a20872fe4233f
Unknown <dajoh16@student.sdu.dk>
2018-03-12 09:58:14 +0100

    Bundle Creation

---
+++ dk.grp1.tanks.enemy/src/main/java/dk/grp1/tanks/enemy/internal/ExampleActivator.java
@@ -0,0 +1,37 @@
+package dk.grp1.tanks.enemy.internal;
+
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+/**
+ * Extension of the default OSGi bundle activator
+ */
+public final class ExampleActivator
+    implements BundleActivator
+{
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STARTING dk.grp1.tanks.enemy" );
+
+    }
+
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STOPPING dk.grp1.tanks.enemy" );
+
+        // no need to unregister our service - the OSGi framework handles it for us
+    }
+}
+
