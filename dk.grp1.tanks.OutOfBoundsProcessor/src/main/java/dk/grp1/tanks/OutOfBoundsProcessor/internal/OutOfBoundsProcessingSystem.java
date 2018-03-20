69434bf13ff5bc786e60950aa8ba6a60c565e397
Barrow <mabar16@student.sdu.dk>
2018-03-20 11:03:00 +0100

    Entities are removed

---
+++ dk.grp1.tanks.OutOfBoundsProcessor/src/main/java/dk/grp1/tanks/OutOfBoundsProcessor/internal/OutOfBoundsProcessingSystem.java
@@ -0,0 +1,37 @@
+package dk.grp1.tanks.OutOfBoundsProcessor.internal;
+
+import dk.grp1.tanks.common.data.Entity;
+import dk.grp1.tanks.common.data.GameData;
+import dk.grp1.tanks.common.data.World;
+import dk.grp1.tanks.common.data.parts.PositionPart;
+import dk.grp1.tanks.common.services.IPostEntityProcessingService;
+
+import java.util.HashSet;
+
+public class OutOfBoundsProcessingSystem implements IPostEntityProcessingService {
+
+    private HashSet<Entity> entitiesToRemove = new HashSet<>();
+    @Override
+    public void postProcess(World world, GameData gameData) {
+
+        //Mark the entities to be removed
+        for (Entity entity: world.getEntities()) {
+            PositionPart positionPart = entity.getPart(PositionPart.class);
+
+            if (positionPart.getX() < 0 || positionPart.getX() > gameData.getGameWidth()){
+                System.out.println("REMOVING ENTITY: " + entity.getID());
+                entitiesToRemove.add(entity);
+            }
+        }
+
+        //Remove the marked entities
+        for (Entity entity: entitiesToRemove) {
+            System.out.println("REMOVED ENTITY: " + entity.getID());
+            world.removeEntity(entity.getID());
+        }
+
+        //Clear the list of removed entities
+        entitiesToRemove.clear();
+
+    }
+}
