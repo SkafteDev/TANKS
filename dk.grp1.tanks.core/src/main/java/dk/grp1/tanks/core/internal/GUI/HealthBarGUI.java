2169b93c2c2bdfe9b72f9823ae410f2c71734bdf
JV <jving16@student.sdu.dk>
2018-04-10 13:48:13 +0200

    Healthbar new design

--- dk.grp1.tanks.core/src/main/java/dk/grp1/tanks/core/internal/GUI/HealthBarGUI.java
+++ dk.grp1.tanks.core/src/main/java/dk/grp1/tanks/core/internal/GUI/HealthBarGUI.java
@@ -2,21 +2,19 @@ package dk.grp1.tanks.core.internal.GUI;
 
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import dk.grp1.tanks.common.data.Entity;
 import dk.grp1.tanks.common.data.GameData;
 import dk.grp1.tanks.common.data.World;
 import dk.grp1.tanks.common.data.parts.CirclePart;
 import dk.grp1.tanks.common.data.parts.LifePart;
-import dk.grp1.tanks.common.services.IPostEntityProcessingService;
 
 public class HealthBarGUI implements IGuiProcessingService {
     private Texture healthBarTexture;
-    private Pixmap pix;
+    private Pixmap pixCurrentHealth;
+    private Pixmap pixLostHealth;
     private float barHeight = 2;
     private float barWidth = 4;
     private float xOffSet = 2;
@@ -35,11 +33,6 @@ public class HealthBarGUI implements IGuiProcessingService {
 
     }
 
- //   @Override
-  //  public void setCam(OrthographicCamera camera) {
-  //      this.camera = camera;
-  //  }
-
 
     /**
      * Draw and colour a health bar based on entity position and remaining health
@@ -50,26 +43,31 @@ public class HealthBarGUI implements IGuiProcessingService {
      * @param healthValue current/max ratio
      */
     private void makeHealthBar(float x, float y, float radius, float healthValue, SpriteBatch batch) {
-        pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixCurrentHealth = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
 
         //Colour health bar based on health value
-        if (healthValue > 0.75) {
-            pix.setColor(Color.GREEN);
-        } else if (healthValue > 0.5) {
-            pix.setColor(Color.YELLOW);
-        } else if (healthValue > 0.25) {
-            pix.setColor(Color.ORANGE);
-        } else {
-            pix.setColor(Color.RED);
-        }
-        pix.fill();
-        healthBarTexture = new Texture(pix);
+        pixLostHealth = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixLostHealth.setColor(Color.RED);
+        pixCurrentHealth.setColor(Color.GREEN);
+
+        pixCurrentHealth.fill();
+        pixLostHealth.fill();
+
+        healthBarTexture = new Texture(pixCurrentHealth);
+        Texture healthBarLostTexture = new Texture(pixLostHealth);
+
         batch.begin();
+
         batch.draw(healthBarTexture, x - xOffSet * radius, y + yOffSet * radius,
                 radius * barWidth * healthValue, barHeight);
+        batch.draw(healthBarLostTexture, (x - xOffSet * radius)+ (radius * barWidth * healthValue), y + yOffSet * radius,
+                radius * barWidth * (1-healthValue), barHeight);
+
         batch.end();
-        pix.dispose();
+        pixLostHealth.dispose();
+        pixCurrentHealth.dispose();
         healthBarTexture.dispose();
+        healthBarLostTexture.dispose();
     }
 
 }
