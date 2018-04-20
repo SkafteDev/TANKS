08493fb5e638d79d72fc034504d6dc2ae0a63822
Vic <victor.s@live.dk>
2018-04-20 14:12:45 +0200

    GoalSelector WIP

--- dk.grp1.tanks.weapon.HomingMissile/src/main/java/dk/grp1/tanks/weapon/HomingMissile/internal/AI/GoalSelector.java
+++ dk.grp1.tanks.weapon.HomingMissile/src/main/java/dk/grp1/tanks/weapon/HomingMissile/internal/AI/GoalSelector.java
@@ -1,23 +1,50 @@
 package dk.grp1.tanks.weapon.HomingMissile.internal.AI;
 
 import dk.grp1.tanks.common.data.Entity;
+import dk.grp1.tanks.common.data.GameData;
 import dk.grp1.tanks.common.data.World;
+import dk.grp1.tanks.common.utils.Vector2D;
+
+import java.util.List;
 
 public class GoalSelector implements IGoalSelector{
 
     private final World world;
+    private GameData gameData;
     private final Entity origin;
-    private final float explosionRadius;
+    private Entity homingMissile;
+    private final int numberOfPoints = 512;
 
-    public GoalSelector(World world, Entity origin, float explosionRadius){
+    public GoalSelector(World world, GameData gameData, Entity origin, Entity homingMissile){
 
         this.world = world;
+        this.gameData = gameData;
         this.origin = origin;
-        this.explosionRadius = explosionRadius;
+        this.homingMissile = homingMissile;
     }
 
-    public State calculateGoalState(){
+    public State calculateGoalState() {
+        List<Vector2D> vertices = world.getGameMap().getVertices(0, gameData.getGameWidth(), numberOfPoints);
+        Vector2D bestExplosion;
+        int bestCount = 0;
+        for (int i = 0; i < vertices.size(); i++) {
+            int count = 0;
+            float x = vertices.get(i).getX();
+            float y = vertices.get(i).getY() + 1;
+            for (Entity entity : world.getEntities()) {
+                if (isClose(entity, x, y)) {
+                    count++;
+                }
+            }
+            if (count > bestCount) {
+                bestCount = count;
+                bestExplosion = new Vector2D(x, y);
+            }
+        }
 
-        throw new UnsupportedOperationException();
+        State goalState = new State(world.getGameMap(),)
+
+
     }
+
 }
