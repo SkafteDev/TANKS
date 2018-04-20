4b2f7e2105d6bea5809069c6f43e66423a8084ad
Vic <victor.s@live.dk>
2018-04-20 14:00:50 +0200

    CHANGE To INTERFACE

--- dk.grp1.tanks.weapon.HomingMissile/src/main/java/dk/grp1/tanks/weapon/HomingMissile/internal/AI/IGoalSelector.java
+++ dk.grp1.tanks.weapon.HomingMissile/src/main/java/dk/grp1/tanks/weapon/HomingMissile/internal/AI/IGoalSelector.java
@@ -4,5 +4,5 @@ import dk.grp1.tanks.common.data.Entity;
 import dk.grp1.tanks.common.data.World;
 
 public interface IGoalSelector {
-    public State calculateGoalState(World world, Entity origin);
+    public State calculateGoalState();
 }
