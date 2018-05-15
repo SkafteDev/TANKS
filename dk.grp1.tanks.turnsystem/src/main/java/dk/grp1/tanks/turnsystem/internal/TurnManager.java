044f9247a7b74527e1ed5540ecc15ee50bf376d9
JV <jving16@student.sdu.dk>
2018-05-15 20:12:44 +0200

    TurnManager JavaDoc Done

--- dk.grp1.tanks.turnsystem/src/main/java/dk/grp1/tanks/turnsystem/internal/TurnManager.java
+++ dk.grp1.tanks.turnsystem/src/main/java/dk/grp1/tanks/turnsystem/internal/TurnManager.java
@@ -14,6 +14,10 @@ import dk.grp1.tanks.common.services.IRoundService;
 
 import java.util.*;
 
+/**
+ * Responsible for controlling which entity is allowed to make a move,
+ * and change between the entities that are allowed to play.
+ */
 public class TurnManager implements IRoundService, IPostEntityProcessingService, IEventCallback {
 
     private float roundDuration = 30;
@@ -33,7 +37,7 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
     }
 
     /**
-     * All
+     * Results in: All registered entities are allowed to make a move.
      */
     public void stop() {
         for (Entity entity : entities) {
@@ -57,25 +61,30 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
         }
     }
 
+    /**
+     * Give the turn to the next entity
+     * @param source
+     */
     private void selectNextEntity(Entity source) {
 
         if (!entities.contains(source)) {
             return;
         }
+
         timeRemaining = roundDuration;
         int nextIndex = 0;
-        int index = entities.indexOf(source);
+        int index = entities.indexOf(source); // Get the index of the given entity.
         if (index != entities.size() - 1) {
-            nextIndex = index + 1;
+            nextIndex = index + 1;  // Calculate the next index.
         }
 
         for (Entity entity : entities) {
             TurnPart turnPart = entity.getPart(TurnPart.class);
             if (turnPart != null) {
                 if (entities.indexOf(entity) == nextIndex) {
-                    turnPart.setMyTurn(true);
+                    turnPart.setMyTurn(true);           // Sets the turn to true for the next entity
                     currentEntity = entity;
-                } else {
+                } else {                                // And all other entities' TurnPart is set to false.
                     turnPart.setMyTurn(false);
                 }
             }
@@ -87,6 +96,7 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
         if (world == null || gameData == null) {
             throw new IllegalArgumentException("GameData or World is null");
         }
+        // Register all entities in the world
         for (Entity entity : world.getEntities()) {
 
             register(entity);
@@ -94,17 +104,22 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
 
 
         if (wantToEndTurn) {
+            // The current Entity wants to end its turn.
             if (anythingMoves(world)) {
+                // If anything is still moving - e.g. a bullet
                 TurnPart turnPart = currentEntity.getPart(TurnPart.class);
                 if (turnPart != null) {
+                    // Set the current Entity's TurnPart to null. (No entities are allowed to make a move)
                     turnPart.setMyTurn(false);
                 }
                 return;
             }
+            // If nothing moves, then select the next Entity.
             selectNextEntity(currentEntity);
             wantToEndTurn = false;
 
         }
+
         unRegisterDeadEntities();
         unRegisterMissingEntities(world);
 
@@ -118,7 +133,8 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
     }
 
     /**
-     * Unregister entities that are missing, which means entities that are out of bounds, from the Turn Manager
+     * Unregister entities that are missing, which means entities that are out of bounds, from the Turn Manager.
+     * If the current entity is missing, then the system will select the next entity.
      * @param world
      */
 
@@ -136,7 +152,8 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
     }
 
     /**
-     * Unregister entities that are dead from the Turn Manager
+     * Unregister entities that are dead from the Turn Manager.
+     * If the current entity is dead, then the system will select the next entity.
      */
     private void unRegisterDeadEntities() {
         List<Entity> entitiesToRemove = new ArrayList<>();
@@ -156,7 +173,8 @@ public class TurnManager implements IRoundService, IPostEntityProcessingService,
     }
 
     /**
-     * Register a given Entity to the Turn Manager
+     * Register a given Entity to the Turn Manager.
+     * Checks if the entity is already registered.
      * @param entity
      */
     private void register(Entity entity) {
