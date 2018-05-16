b5fe0fff303cb292b512d86d76a7fef6b95f8ae7
Vic <victor.s@live.dk>
2018-05-16 20:22:50 +0200

    Revert "Auto stash before merge of "develop" and "origin/develop""
    This reverts commit 68e1f955ff0dbb78be22f30a62955c9d7a7de627.

--- dk.grp1.tanks.common/src/main/java/dk/grp1/tanks/common/utils/Vector2D.java
+++ dk.grp1.tanks.common/src/main/java/dk/grp1/tanks/common/utils/Vector2D.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * This class represents a mathematical two-dimentional vector
- */
 public class Vector2D {
     private float x;
     private float y;
@@ -16,34 +13,19 @@ public class Vector2D {
         this.y = y;
     }
 
-    /**
-     * Returns the x-component of the vector
-     * @return
-     */
     public float getX() {
         return this.x;
     }
 
-    /**
-     * Returns the y-component of the vector
-     * @return
-     */
     public float getY() {
         return this.y;
     }
 
-    /**
-     * Sets the x-component of the vector
-     * @return
-     */
     public void setX(float x) {
         this.x = x;
     }
 
-    /**
-     * Sets the y-component of the vector
-     * @return
-     */
+
     public void setY(float y) {
         this.y = y;
     }
@@ -80,16 +62,16 @@ public class Vector2D {
 
 
     /**
-     * Rotates the vector 90 degrees, so that a vector pointing downward will now point up
-     * @return the rotated vector
+     * Rotate this vector 90 degrees
+     * @return
      */
     public Vector2D rotate90degrees() {
         return new Vector2D(-y+0.0f, x);
     }
 
     /**
-     * Rotate the vector 90 degrees such that a vector pointing right will now point left
-     * @return the rotated vector
+     * Rotate this vector 90 degrees Clockwise
+     * @return
      */
     public Vector2D rotateClockwise90degrees() {
         return new Vector2D(y, -x+0.0f);
